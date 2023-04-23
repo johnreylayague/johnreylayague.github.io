f6e65cef2272ee82e19ae98aa8897f02f0ff8a7f
Johnrey E. Layaque <layaquejohnrey@gmail.com>
2023-04-23 21:43:53 +0800

    fixed localstorage

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,40 @@
-$( document ).ready(function() { 
+
     
-    $('.career_list > ul > li > a').click(function(e) {
-        e.preventDefault();
-        
-        let id = e.currentTarget.id;
-        let container = $('.career_description');
-        let scrollTo = $('section#'+id+'');
-        let divHeight = $('section#'+id+'');
+$('.career_list > ul > li > a').click(function(e) {
+    e.preventDefault();
+    
+    let id = e.currentTarget.id;
+    let container = $('.career_description');
+    let scrollTo = $('section#'+id+'');
+    let divHeight = $('section#'+id+'');
 
-        // Calculating new position of scrollbar
-        let position = scrollTo.offset().top - container.offset().top  + container.scrollTop();
+    // Calculating new position of scrollbar
+    let position = scrollTo.offset().top - container.offset().top  + container.scrollTop();
 
-            $('.career_description').css({height:divHeight.height()});
-            $('.career_list>ul>li>a').removeClass('selected');
-            $('#'+id+'').addClass('selected');
+        $('.career_description').css({height:divHeight.height()});
+        $('.career_list>ul>li>a').removeClass('selected');
+        $('#'+id+'').addClass('selected');
 
-            // Setting the value of scrollbar
-            container.scrollTop(position);
+        // Setting the value of scrollbar
+        container.scrollTop(position);
 
-            localStorage.setItem('career_id', id);
-    });
+        localStorage.setItem('career_id', id);
+});
 
-    $(window).on('resize', function(e){
+$(window).on('resize', function(e){
 
-        let id = $('.career_list > ul > li > a.selected').attr('id');
+    let id = $('.career_list > ul > li > a.selected').attr('id');
 
-            $("#"+id+"").click();
-    });
+        $("#"+id+"").click();
+});
 
-    $(window).on('load', function(){
-        let id = $('.career_list > ul > li > a.selected').attr('id');
-            
-            if (localStorage.career_id != null) { 
-                $('#'+localStorage.getItem('career_id')+'').click();
-            } else { 
-                $('.career_list > ul > li > a#proweaver').click();
-            }
-            
-    });
+$(window).on('load', function(){
+
+    if (localStorage.career_id != null) { 
+        $('#'+localStorage.getItem('career_id')+'').addClass('selected');
+    } else { 
+        $('.career_list > ul > li > a#proweaver').addClass('selected');
+        localStorage.setItem('career_id','proweaver');
+    }
+    
 });
