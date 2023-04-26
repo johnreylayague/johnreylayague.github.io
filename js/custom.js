ad4456cb17564f9f23a61b39e19f635037d1525c
Johnrey E. Layaque <layaquejohnrey@gmail.com>
2023-04-26 22:42:37 +0800

    updated onload localstorage, added new company part time job

--- js/custom.js
+++ js/custom.js
@@ -1,69 +1,87 @@
 
     
-$('.career_list > ul > li > a').click(function(e) {
-    e.preventDefault();
-    
-    let id = e.currentTarget.id;
-    let container = $('.career_description');
-    let scrollTo = $('section#'+id+'');
-    let divHeight = $('section#'+id+'');
+$( document ).ready(function() {
 
-    // Calculating new position of scrollbar
-    let position = scrollTo.offset().top - container.offset().top  + container.scrollTop();
+    $('.career_list > ul > li > a').click(function(e) {
+        e.preventDefault();
+        
+        let id = e.currentTarget.id;
+        let container = $('.career_description');
+        let scrollTo = $('section#'+id+'');
+        let divHeight = $('section#'+id+'');
 
-        $('.career_description').css({height:divHeight.height()});
-        $('.career_list>ul>li>a').removeClass('selected');
-        $('#'+id+'').addClass('selected');
+        // Calculating new position of scrollbar
+        let position = scrollTo.offset().top - container.offset().top  + container.scrollTop();
 
-        // Setting the value of scrollbar
-        container.scrollTop(position);
+            $('.career_description').css({height:divHeight.height()});
+            $('.career_list>ul>li>a').removeClass('selected');
+            $('#'+id+'').addClass('selected');
 
-        localStorage.setItem('career_id', id);
-});
+            // Setting the value of scrollbar
+            container.scrollTop(position);
 
-$(window).on('resize', function(e){
+            localStorage.setItem('career_id', id);
+    });
 
-    let id = $('.career_list > ul > li > a.selected').attr('id');
-        $("#"+id+"").click();
+    $(window).on('resize', function(e){
 
-});
+        let id = $('.career_list > ul > li > a.selected').attr('id');
+            $("#"+id+"").click();
 
-$(window).on('load', function(){
+    });
 
-    let divHeight = $('section#'+localStorage.career_id+'');
+    $(window).on('load', function(){
 
-        if (localStorage.career_id != null) { 
-                $('.career_description').css({height:divHeight.height()});
+        let divHeight = $('section#'+localStorage.career_id+'');
+        let i = false;
+
+            $('.career_list > ul > li > a').each(function(key, value){
+                if(localStorage.career_id != "" && localStorage.career_id == value.id ) {
+                i = true;
+                }
+            });
+
+            if(i == true) {
                 $('#'+localStorage.getItem('career_id')+'').addClass('selected');
-                $("#"+localStorage.career_id+"").click();
-        } else { 
-                $('.career_list > ul > li').first().find('a').addClass('selected');
-                $('.career_list > ul > li').first().find('a').css({height:divHeight.height()});
-                localStorage.setItem('career_id', $('.career_list > ul > li').first().find('a').attr('id'));
-                $("#proweaver").click();
-        }
-        // theme changed onload page
-        if( localStorage.getItem('theme_style') == 'dark'){
+            } else {
+                $('.career_list > ul > li').first().find('a').click();
+            }
+
+            if (localStorage.career_id != "") { 
+                    $('.career_description').css({height:divHeight.height()});
+                    $('#'+localStorage.getItem('career_id')+'').addClass('selected');
+                    $("#"+localStorage.career_id+"").click();
+            } else { 
+                    $('.career_list > ul > li').first().find('a').addClass('selected');
+                    $('.career_list > ul > li').first().find('a').css({height:divHeight.height()});
+                    localStorage.setItem('career_id', $('.career_list > ul > li').first().find('a').attr('id'));
+                    $('.career_list > ul > li').first().find('a').attr('id').click();
+
+            }
+            // theme changed onload page
+            if( localStorage.getItem('theme_style') == 'dark'){
+                $('body').removeClass('light').addClass('dark');
+                $('#theme-change').find('span').removeClass('moon-icon').addClass('sun-icon');
+            } else if( localStorage.getItem('theme_style') == 'light'){
+                $('body').removeClass('dark').addClass('light');
+                $('#theme-change').find('span').removeClass('sun-icon').addClass('moon-icon');
+            }
+
+    });
+
+    $('#theme-change').click(function(e) { 
+        e.preventDefault();
+
+        if($('body').hasClass("light")) { 
             $('body').removeClass('light').addClass('dark');
             $('#theme-change').find('span').removeClass('moon-icon').addClass('sun-icon');
-        } else if( localStorage.getItem('theme_style') == 'light'){
+            localStorage.setItem('theme_style','dark');
+        } else {
             $('body').removeClass('dark').addClass('light');
             $('#theme-change').find('span').removeClass('sun-icon').addClass('moon-icon');
+            localStorage.setItem('theme_style','light');
         }
 
+    });
 });
 
-$('#theme-change').click(function(e) { 
-    e.preventDefault();
-
-    if($('body').hasClass("light")) { 
-        $('body').removeClass('light').addClass('dark');
-        $('#theme-change').find('span').removeClass('moon-icon').addClass('sun-icon');
-        localStorage.setItem('theme_style','dark');
-    } else {
-        $('body').removeClass('dark').addClass('light');
-        $('#theme-change').find('span').removeClass('sun-icon').addClass('moon-icon');
-        localStorage.setItem('theme_style','light');
-    }
-
-});
